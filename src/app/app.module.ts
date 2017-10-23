63114f8667b4d8a264398313179426c06b8a7c79
BRPickens <brpickens@gmail.com>
2017-10-23 17:39:55 -0500

    do the chirpForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule, MatCardContent } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ChirpFormComponent } from './chirp-form/chirp-form.component';
+import { ListComponent } from './list/list.component';
+import { SingleComponent } from './single/single.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChirpFormComponent,
+    ListComponent,
+    SingleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
