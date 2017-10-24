cb75355b7a934cc05fca6c55e68b9631aceb38d2
BRPickens <brpickens@gmail.com>
2017-10-23 19:28:32 -0500

    fist time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule, MatCardContent } from '@angular/material';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule, MatCardContent, MatToolbarModule, MatMenuModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ChirpFormComponent } from './chirp-form/chirp-form.component';
@@ -21,7 +21,9 @@ import { SingleComponent } from './single/single.component';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
