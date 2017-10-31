f99263a76fe978dcf6dff649162441c1255fc5a1
BRPickens <brpickens@gmail.com>
2017-10-31 12:23:59 -0500

    style it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,10 @@ RouterModule.forRoot([
   }
 ])
 
-interface ICHirp {
-
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  chirps = [
-
-  ]
-
-  user: 'Black Joker';
 }
